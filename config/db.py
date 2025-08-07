f14c7e8c1cf5306afbfdc5594269840efbbd2638
swati <tiwariswati93@gmail.com>
2025-08-07 19:43:18 +0530

    feat:mongodb connection

--- config/db.py
+++ config/db.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_DB_URL")
-DB_NAME = os.getenv("MONGO_DB_NAME")
+MONGO_URI = os.getenv("MONGO_DB_URL") # Check Variable Name in ENV File
+DB_NAME = os.getenv("MONGO_DB_NAME") # Check Variable Name in ENV File
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
+movies_collection = db.get_collection("movies")
