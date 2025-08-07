a7fe421635baa60bb33853ad82c3ff2cc469e751
Ashish <ashishdash.1801@gmail.com>
2025-08-07 17:59:32 +0530

    feat: local db, exact pkg versions

--- config/db.py
+++ config/db.py
@@ -1,17 +1,11 @@
-# config/db.py
 from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
-MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "movie_recommender")
+MONGO_URI = os.getenv("MONGO_DB_URL")
+DB_NAME = os.getenv("MONGO_DB_NAME")
 
 client = AsyncIOMotorClient(MONGO_URI)
-db = client[MONGO_DB_NAME]
-
-# Access collections
-user_collection = db.get_collection("users")
-movie_collection = db.get_collection("movies")
-watchlist_collection = db.get_collection("user_watchlist")
+db = client[DB_NAME]
